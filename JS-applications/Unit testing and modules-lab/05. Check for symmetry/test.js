530aefe8ff2a578687cbd6e749628f5ca045a1e6
Dimitar.Petroww <Mitko66692@abv.bg>
2020-12-19 22:02:03 +0200

    add problem 05

---
+++ JS-applications/Unit testing and modules-lab/05. Check for symmetry/test.js
@@ -0,0 +1,15 @@
+const isSymmetric=require("./app")
+let { assert }=require("chai")
+
+
+describe("function isSymmetric", ()=> {
+    it("function should return false if we pass something different from array as an argument" , () => {
+        assert.equal(isSymmetric(5), false)
+    })
+    it("function should return false if the array is not symmetric" , () => {
+        assert.equal(isSymmetric([1, 2, 3, 4, 5]), false)
+    })
+    it("function should return true if the array is symmetric", () => {
+        assert.equal(isSymmetric([1, 1, 1]), true)
+    })
+})
