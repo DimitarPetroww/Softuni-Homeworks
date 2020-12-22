99ad26c2c41f334021f620b6dfcc5aa2de1e16ec
Dimitar Petrov <mitko66692@abv.bg>
2020-12-22 19:35:28 +0200

    Update 08. Tickets.js

--- JS-Advanced/Exercise_ Objects & Classes/08. Tickets.js
+++ JS-Advanced/Exercise_ Objects & Classes/08. Tickets.js
@@ -1,14 +1,12 @@
 function solve(arr , sortCriteria) {
     class Ticket {
         constructor(destination , price , status) {
-            this.destination=destination
-            this.price=Number(price)
-            this.status=status
+            Object.assign(this, {destination, price: Number(price), status})
         }
     }
     return arr.map(x=> x.split("|"))
     .reduce((a, [x, y, z]) => {
         a.push(new Ticket(x, y, z))
         return a
-    }).sort((a,b) => sortCriteria==="price" ? a.price-b.price : a[sortCriteria].localeCompare(b[sortCriteria]))
+    }, []).sort((a,b) => sortCriteria==="price" ? a.price-b.price : a[sortCriteria].localeCompare(b[sortCriteria]))
 }
