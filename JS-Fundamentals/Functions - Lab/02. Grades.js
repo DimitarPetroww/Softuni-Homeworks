6d102e8b2c53644ed2796479515e0bed7c81e689
Dimitar Petrov <mitko66692@abv.bg>
2020-12-22 20:14:39 +0200

    Create 02. Grades.js

---
+++ JS-Fundamentals/Functions - Lab/02. Grades.js
@@ -0,0 +1,17 @@
+function solve(grade) {
+    let result = ""
+    let gradeType = ""
+    if (grade >= 2.00 && grade <= 2.99) {
+        gradeType = "Fail"
+    } else if (grade >= 3.00 && grade <= 3.49) {
+        gradeType = "Poor"
+    } else if (grade >= 3.50 && grade <= 4.49) {
+        gradeType = "Good"
+    } else if (grade >= 4.50 && grade <= 5.49) {
+        gradeType = "Very good"
+    } else if (grade >= 5.50 && grade <= 6.00) {
+        gradeType = "Excellent"
+    }
+    return gradeType
+    
+}
