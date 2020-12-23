7b21bcf3d3d87b9b24557559715480169e9534d1
Dimitar.Petroww <Mitko66692@abv.bg>
2020-12-23 18:46:26 +0200

    Update app.js

--- JS-applications/REST Services and AJAX-lab/05.Github-Repos/scripts/app.js
+++ JS-applications/REST Services and AJAX-lab/05.Github-Repos/scripts/app.js
@@ -1,13 +1,14 @@
 function loadRepos() {
-	const httpRequest=new XMLHttpRequest() 
 	const user=document.getElementById("username")
 	const url=`https://api.github.com/users/${user.value}/repos`
 	const repositories=document.getElementById("repos")
  
-	httpRequest.addEventListener("loadend", function() {
-	   let repos=JSON.parse(this.responseText)
-	   repositories.innerHTML=repos.map(x=>`<li><a href="${x.html_url}"target="_blank">${x.name}</a></li>`).join("")
+	fetch(url)
+	.then(response => response.json())
+	.then(function(repos) {
+		repositories.innerHTML=repos.map(x=>`<li><a href="${x.html_url}"target="_blank">${x.name}</a></li>`).join("")
 	})
-	httpRequest.open("GET", url)
-	httpRequest.send()
+	.catch(function() {
+		repositories.innerHTML=`<li>Error</li>`
+	})
 }
