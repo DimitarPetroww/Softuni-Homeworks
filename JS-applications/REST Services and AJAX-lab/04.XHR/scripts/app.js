38a31052e95db995f7fe4500e038dd45db295179
Dimitar.Petroww <Mitko66692@abv.bg>
2020-12-23 18:11:37 +0200

    Create 04.Problem

---
+++ JS-applications/REST Services and AJAX-lab/04.XHR/scripts/app.js
@@ -0,0 +1,12 @@
+function loadRepos() {
+   const httpRequest=new XMLHttpRequest() 
+   const url=`https://api.github.com/users/DimitarPetroww/repos`
+   const result=document.getElementById("res")
+
+   httpRequest.addEventListener("loadend", function() {
+      let repos=JSON.parse(this.responseText)
+      result.innerHTML=repos.map(x=> `<p><a href="${x.svn_url}">${x.name}</a></p>`).join("")
+   })
+   httpRequest.open("GET", url)
+   httpRequest.send()
+}
