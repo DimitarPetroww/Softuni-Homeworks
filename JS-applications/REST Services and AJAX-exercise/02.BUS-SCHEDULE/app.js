df2d84db62d2a01f00594f3789cbd45b3a4ab160
Dimitar.Petroww <Mitko66692@abv.bg>
2021-01-03 19:15:06 +0200

    Update app.js

--- JS-applications/REST Services and AJAX-exercise/02.BUS-SCHEDULE/app.js
+++ JS-applications/REST Services and AJAX-exercise/02.BUS-SCHEDULE/app.js
@@ -1,11 +1,29 @@
 function solve() {
-
+    let url = `https://judgetests.firebaseio.com/schedule/depot.json`
+    const [departBtn, arriveBtn] = document.querySelectorAll("#depart, #arrive")
+    let info = document.getElementById("info").firstChild
+    let stopName;
     function depart() {
-        console.log('Depart TODO...');
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                info.textContent = `Next stop ${data.name}`
+                url = `https://judgetests.firebaseio.com/schedule/${data.next}.json`
+                stopName = data.name
+                departBtn.setAttribute("disabled", true)
+                arriveBtn.removeAttribute("disabled")
+            })
+            .catch(() => {
+                info.textContent = `Error`
+                departBtn.setAttribute("disabled", true)
+                arriveBtn.setAttribute("disabled", true)
+            })
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        info.textContent = `Arriving at ${stopName}`
+        arriveBtn.setAttribute("disabled", true)
+        departBtn.removeAttribute("disabled")
     }
 
     return {
