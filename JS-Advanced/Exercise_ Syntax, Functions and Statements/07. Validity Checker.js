cc47dc6fb84d5c16bd0439bfd361ce2d0c68c437
Dimitar Petrov <mitko66692@abv.bg>
2021-01-10 14:16:54 +0200

    Update 07. Validity Checker.js

--- JS-Advanced/Exercise_ Syntax, Functions and Statements/07. Validity Checker.js
+++ JS-Advanced/Exercise_ Syntax, Functions and Statements/07. Validity Checker.js
@@ -1,17 +1,13 @@
-function solve(arr) {
-  let x1=arr.shift()
-  let y1=arr.shift()
-  let x2=arr.shift()
-  let y2=arr.shift()
-  console.log(`{${x1}, ${y1}} to {0, 0} is ${checkValidity(x1 , y2 , 0 , 0)}`);
-  console.log(`{${x2}, ${y2}} to {0, 0} is ${checkValidity(x2 , y2 , 0 , 0)}`);
-  console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is ${checkValidity(x1 , y1 , x2, y2)}`);
-  function checkValidity(x1 , y1 , x2 , y2) {
-      let value=Math.sqrt((x2 -x1) **2 + (y2 - y1)**2)
-      if(Number.isInteger(value)) {
-          return "valid"
-      }else {
-          return "invalid"
-      }
-  }
+function solve(x1, y1, x2, y2) {
+    return  [`{${x1}, ${y1}} to {0, 0} is ${checkValidity(x1, y2, 0, 0)}`,
+    `{${x2}, ${y2}} to {0, 0} is ${checkValidity(x2, y2, 0, 0)}`,
+    `{${x1}, ${y1}} to {${x2}, ${y2}} is ${checkValidity(x1, y1, x2, y2)}`].join("\n")
+    function checkValidity(x1, y1, x2, y2) {
+        let value = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        if (Number.isInteger(value)) {
+            return "valid"
+        } else {
+            return "invalid"
+        }
+    }
 }
