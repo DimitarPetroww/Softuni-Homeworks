d2cdf2345d91512a9dc19a113f56bfb3158be61a
Dimitar Petrov <mitko66692@abv.bg>
2021-01-10 14:07:21 +0200

    Update and rename 02. Math Operations.js to 05. Math Operations.js

---
+++ JS-Advanced/Lab_ Syntax, Functions and Statements/05. Math Operations.js
@@ -0,0 +1,12 @@
+function solve(n1 , n2 , operator) {
+   // '+', '-', '*', '/', '%', '**'. 
+  	let obj={
+		"+": (n1, n2) => n1 + n2,
+      	"-": (n1, n2) => n1 - n2,
+      	"*": (n1, n2) => n1 * n2,
+      	"/": (n1, n2) => n1 / n2,
+      	"%": (n1, n2) => n1 % n2,
+      	"**": (n1, n2) => n1 ** n2,
+    }
+    return obj[operator](n1, n2)
+}
