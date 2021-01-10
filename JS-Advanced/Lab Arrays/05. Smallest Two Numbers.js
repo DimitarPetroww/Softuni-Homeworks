8d039e251e13fc953700b1fa395bd4502d40737d
Dimitar Petrov <mitko66692@abv.bg>
2021-01-10 14:58:12 +0200

    Update and rename 06. Smallest Two Numbers.js to 05. Smallest Two Numbers.js

---
+++ JS-Advanced/Lab Arrays/05. Smallest Two Numbers.js
@@ -0,0 +1,3 @@
+function solve(arr) {
+    return arr.sort((a , b)=> a - b).splice(0,2).join(" ")
+}
--- JS-Advanced/Lab Arrays/06. Smallest Two Numbers.js
+++
@@ -1,3 +0,0 @@
-function solve(arr) {
-    console.log(arr.sort((a , b)=> a - b).splice(0,2).join(" "));
-}
