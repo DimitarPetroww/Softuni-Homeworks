0a6f5b99cbabdaf270cd355f098df2bd7d497ff0
Dimitar Petrov <mitko66692@abv.bg>
2021-01-10 14:03:03 +0200

    Update and rename 01.String Length.js to 02.String Length.js

--- JS-Advanced/Lab_ Syntax, Functions and Statements/01.String Length.js
+++
@@ -1,4 +0,0 @@
-function solve(...strings) {
-    console.log(strings.map(x=> x.length).reduce((a , b) => a + b  , 0));
-    console.log(Math.floor((strings.map(x=> x.length).reduce((a , b) => a + b  , 0)) / strings.length))
-}
