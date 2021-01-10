b1b55477d6f6cc071a5b03aa02bd8b32251be17c
Dimitar Petrov <mitko66692@abv.bg>
2021-01-10 15:08:15 +0200

    Update and rename 05. Process Odd Numbers.js to 08. Process Odd Positions.js

--- JS-Advanced/Lab Arrays/05. Process Odd Numbers.js
+++
@@ -1,7 +0,0 @@
-function solve(arr) {
-    console.log(arr.filter((x , i)=> {
-        if(i % 2 !==0) {
-            return x.toString()
-        }
-    }).reverse().map(x=>x*2).join(" "));
-}
---
+++ JS-Advanced/Lab Arrays/08. Process Odd Positions.js
@@ -0,0 +1,3 @@
+function solve(arr) {
+    return arr.filter((_ , i)=> i % 2===0).reverse().map(x=>x*2).join(" ")
+}
