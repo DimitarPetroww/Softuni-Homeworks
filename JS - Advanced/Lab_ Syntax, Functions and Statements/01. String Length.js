021526638b71c340af42c5ef988abc8694555ba9
Dimitar.Petroww <Mitko66692@abv.bg>
2021-01-10 16:17:14 +0200

    Delete 01. String Length.js

--- JS - Advanced/Lab_ Syntax, Functions and Statements/01. String Length.js
+++
@@ -1,4 +0,0 @@
-function solve(...strings) {
-    return [strings.map(x=> x.length).reduce((a , b) => a + b  , 0),
-    Math.floor((strings.map(x=> x.length).reduce((a , b) => a + b  , 0)) / strings.length)].join("\n")
-}
