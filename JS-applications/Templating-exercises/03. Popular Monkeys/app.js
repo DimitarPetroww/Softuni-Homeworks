55824e285aa40cd98c7c509274ca9ec18000ec8c
Dimitar.Petroww <Mitko66692@abv.bg>
2021-01-27 17:37:39 +0200

    Update app.js

--- JS-applications/Templating-exercises/03. Popular Monkeys/app.js
+++ JS-applications/Templating-exercises/03. Popular Monkeys/app.js
@@ -1,6 +1,6 @@
 document.querySelector(".monkeys").addEventListener("click", (e) => {
     if (e.target.tagName !== "BUTTON") return
-
+    
     const info=e.target.parentElement.querySelector("p")
     info.style.display = info.style.display !== "block" ? "block" : "none"
 })
