3b4565fd8ff2d1f1c639c2de4db1bc757bc14bf2
Dimitar.Petroww <Mitko66692@abv.bg>
2021-02-02 16:54:19 +0200

    update

---
+++ JS-applications/Routing - lab/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyC_2tUsTqAo0MzvdDLKogJw4tF6uWNx1dU",
+  authDomain: "routing-2c6e9.firebaseapp.com",
+  projectId: "routing-2c6e9",
+  storageBucket: "routing-2c6e9.appspot.com",
+  messagingSenderId: "184892731778",
+  appId: "1:184892731778:web:94bcfa9e17df78d8395f9b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
