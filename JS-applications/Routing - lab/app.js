41c3f4eee9aa9c9c69be39f5e54b2022f512a371
Dimitar.Petroww <Mitko66692@abv.bg>
2021-02-02 17:36:53 +0200

    update

--- JS-applications/Routing - lab/app.js
+++ JS-applications/Routing - lab/app.js
@@ -1,5 +1,22 @@
-export default(() => {
-    const routes = {
-        
+export default (() => {
+    const createFurniture = (e) => {
+        e.preventDefault()
+        const [make, model, year, description, price, image, material] = ["new-make", "new-model", "new-year", "new-description", "new-price", "new-image", "new-material"]
+            .map(id => document.getElementById(id))
+            .map(e => e.value.trim())
+            .map(x=> isNaN(x) ? x : Number(x))
+
+        if (
+            (make.length < 4 || model.length < 4)
+            ||(year > 2050 || year < 1950)
+            || description.length <= 10
+            || price < 0
+            || material === ""
+        ) { console.log("da")}
+
+
     }
+
+    document.querySelector("input[type='submit']").addEventListener("click", createFurniture)
+
 })()
