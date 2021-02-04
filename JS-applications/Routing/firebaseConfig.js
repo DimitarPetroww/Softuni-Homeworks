3be862313f982cf095169f3d898edf8de4d0fc5e
Dimitar Petrov <mitko66692@abv.bg>
2021-02-04 20:24:28 +0200

    Delete JS-applications/Routing directory

--- JS-applications/Routing/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-firebase.initializeApp({
-    apiKey: "AIzaSyC_2tUsTqAo0MzvdDLKogJw4tF6uWNx1dU",
-    authDomain: "routing-2c6e9.firebaseapp.com",
-    databaseURL: "https://routing-2c6e9-default-rtdb.firebaseio.com",
-    projectId: "routing-2c6e9",
-    storageBucket: "routing-2c6e9.appspot.com",
-    messagingSenderId: "184892731778",
-    appId: "1:184892731778:web:94bcfa9e17df78d8395f9b"
-});
--- JS-applications/Routing/scripts/loadPartials.js
+++
@@ -1,6 +0,0 @@
-export default function(context) {
-    return context.loadPartials({
-        'header': './templates/common/header.hbs',
-        'footer': './templates/common/footer.hbs'
-    });
-}
