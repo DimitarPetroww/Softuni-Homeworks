de5e71db674133021bdeaca2b2e26b815e57e4d4
Dimitar Petrov <mitko66692@abv.bg>
2021-02-18 16:22:09 +0200

    Update 1.js

--- JS - Advanced/exams/JS Advanced Exam - 24 Oct 2020/1.js
+++ JS - Advanced/exams/JS Advanced Exam - 24 Oct 2020/1.js
@@ -1,74 +1,65 @@
 function solve() {
-    let [lecture, date, module, btn] = document.querySelectorAll("input, input, select, button")
-    let modules = document.querySelector(".modules")
-    btn.addEventListener("click", (e) => {
+    const [lecture, date, module] = document.querySelectorAll("input, input, select")
+    document.querySelector(".form-control > button").addEventListener("click", addLecture)
+    const modules = document.querySelector(".modules")
+    modules.addEventListener("click", delLecture)
+    function addLecture(e) {
         e.preventDefault()
-        if (!lecture.value || !date.value || module.value === `Select module`) {
+        if (lecture.value == "" || date.value == "" || module.value == "Select module") {
             return
         }
-        let currentModule = Array.from(document.querySelectorAll(".module")).find(x => x.querySelector("h3").textContent === `${module.value.toUpperCase()}-MODULE`)
-        if (currentModule) {
-            addModule(currentModule)
-        } else {
-            createModule()
-        }
+        const found = Array.from(modules.children).find(x => x.firstElementChild.textContent === `${module.value.toUpperCase()}-MODULE`)
+        found ? addModule(found, lecture.value, date.value) : createModule(modules, module.value, lecture.value, date.value)
 
-    })
-    function addModule(currentModule) {
-        let li = document.createElement("li")
-        let h4 = document.createElement("h4")
-        let button = document.createElement("button")
-
-        li.classList.add("flex")
-        button.classList.add("red")
-        button.textContent = "Del"
-
-        h4.textContent = `${lecture.value} - ${formatDate(date.value).join(" - ")}`
-
-        li.appendChild(h4)
-        li.appendChild(button)
-        currentModule.querySelector("ul").appendChild(li)
-
-        Array.from(currentModule.querySelector("ul").children)
-            .sort((a, b) => new Date(a.innerText.split(" - ")[1]) - new Date(b.innerText.split(" - ")[1])).map(x => currentModule.querySelector("ul").appendChild(x))
     }
-    function createModule() {
-        let divModule = document.createElement("div")
-        let h3 = document.createElement("h3")
-        let ul = document.createElement("ul")
-        let li = document.createElement("li")
-        let h4 = document.createElement("h4")
-        let button = document.createElement("button")
-
-        li.classList.add("flex")
-        button.classList.add("red")
-        button.textContent = "Del"
-        
-        h4.textContent = `${lecture.value} - ${formatDate(date.value).join(" - ")}`
-
-        h3.textContent = `${module.value.toUpperCase()}-MODULE`
-
-        li.appendChild(h4)
-        li.appendChild(button)
-        ul.appendChild(li)
-
-        divModule.appendChild(h3)
-        divModule.appendChild(ul)
-        divModule.classList.add("module")
-        divModule.addEventListener("click", deleteElement)
-
-        modules.appendChild(divModule)
-    }
-    function deleteElement(e) {
+    function delLecture(e) {
         if (e.target.tagName !== "BUTTON") {
             return
         }
-        e.target.parentElement.remove()
-        if (e.currentTarget.querySelector("ul").children.length === 0) {
-            e.currentTarget.remove()
+        const li = e.target.parentElement
+        const ul = li.parentElement
+        li.remove()
+        if (ul.children.length === 0) {
+            ul.parentElement.remove()
         }
+    }
+    const append = (parent, ...childrens) => childrens.map(x => parent.appendChild(x))
+    function addModule(found, lecture, date) {
+        const parent = found.querySelector("ul")
+        parent.appendChild(renderLi("", { class: "flex" }, renderH4(`${lecture} - ${formatDate(date).join(" - ")}`, {}), renderButton("Del", { class: "red" })))
+        const sortedChildrens = Array.from(parent.children)
+        .sort((a, b) => new Date(a.querySelector("h4").textContent.split(" - ")[1]) - new Date(b.querySelector("h4").textContent.split(" - ")[1]))
+        append(parent, ...sortedChildrens)
+    }
+    function createModule(modules, module, lecture, date) {
+        modules.appendChild(
+            renderDiv("", { class: "module" },
+                renderH3(`${module.toUpperCase()}-MODULE`, {}),
+                renderUl("", {},
+                    renderLi("", { class: "flex" },
+                        renderH4(`${lecture} - ${formatDate(date).join(" - ")}`, {}),
+                        renderButton("Del", { class: "red" }))))
+        )
+    }
+
+    function render(type, content, attributes, ...childrens) {
+        const element = document.createElement(type)
+
+        if (content !== "") {
+            element.innerHTML = content
+        }
+        Object.entries(attributes).forEach(([key, value]) => element.setAttribute(key, value))
+
+        append(element, ...childrens)
+        return element
     }
     function formatDate(date) {
         return [date.split("T")[0].replace(/-/g, "/"), date.split("T")[1]]
     }
-}
+    const renderDiv = render.bind(undefined, "div")
+    const renderH3 = render.bind(undefined, "h3")
+    const renderUl = render.bind(undefined, "ul")
+    const renderLi = render.bind(undefined, "li")
+    const renderH4 = render.bind(undefined, "h4")
+    const renderButton = render.bind(undefined, "button")
+};
